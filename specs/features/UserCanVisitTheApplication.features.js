c1d4c4e7db408ee3a2aafa528c5ae48d23eaa322
miyeaier <miyesier@gmail.com>
2021-11-16 10:12:18 +0100

    add acceptane test on feture file and write code on index html for we interface ..

--- specs/features/UserCanVisitTheApplication.features.js
+++ specs/features/UserCanVisitTheApplication.features.js
@@ -1,6 +1,48 @@
 describe("User visiting the appliction url", () => {
-  it("is expected to see a header", () => {
-    cy.visit("/");
-    cy.get("h1").should("contain.text","Adress Book")
+   // create some fake data. remember that this string needs to be parsable (using'JSON.parse()')
+   
+   const entries = '[{"name": "John Doe", "phone": "031-121212", "twitter":"@john_doe"}, {"name": "Jane Doe", "phone": "+1 202-121212", "twitter":"@jane_doe"}]';
+   
+   beforeEach(() => {
+    cy.visit("/",{
+      onBeforeload(window){
+        window.localStorage.setItem('entrise','...the data need to store')
+      }
     })
   })
+  cy.get('[name=address_list] ul').as('displayList')
+  
+  }); 
+   
+  it("is expected to see a header", () => {
+    cy.get("h1").should("contain.text","Adress Book")
+   });
+
+  it('is expected to see an address list', () => {
+     cy.get('@displayList').should('exist').and('be.visible')
+  });
+      
+  it('is expected to display 2 entries', () => {
+     cy.get('@displayList').children().should('have.length', 2)
+   });
+
+ 
+   describe('adding an entry by submitting a form', () => {
+    
+    beforeEach(() => {
+      cy.get('[name=name]').type('Jill Doe')
+      cy.get('[name=phone]').type('0700-121212')
+      cy.get('[name=twitter]').type('@jill_doe')
+      cy.get('[name=submit]').click()
+   });
+    
+   it('is expected to display 3 entries', () => {
+      cy.get('@displayList').children().should('have.length', 3)
+    });
+    
+    it('is expected to display the new entry', () => {
+      cy.get('@displayList').should('contain.text', 'Jill Doe')
+    });
+});
+ 
+
---
+++ src/js/app.js
@@ -0,0 +1,29 @@
+const addressBook = new AddressBook();
+const addressList = document.querySelector("[name=address_list] ul");
+const messageElement = document.getElementById("response_message");
+
+const displayEntries = () => {
+  addressList.innerHTML = "";
+    const entries = addressBook.index();
+    let html;
+    
+    entries.forEach((entry) => {
+        html = `Name: ${entry.name} `;
+        html += `Phone: ${entry.phone} `;
+        html += `Twitter: ${entry.twitter}`;
+        let newListItem = document.createElement("li");
+        newListItem.innerHTML = html;
+        addressList.appendChild(newListItem);
+    });
+};
+
+const handleForm = (form) => {
+
+  const name = form.elements.name.value;
+  const phone = form.elements.phone.value;
+  const twitter = form.elements.twitter.value;
+  addressBook.create({ name: name, phone: phone, twitter: twitter });
+  form.reset();
+  displayEntries();
+  return false;
+};
